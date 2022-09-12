d44fd4860490e53f422f9ef808d019cbd2e1c6f0
Bello aisha <belloaishaaderonke@gmail.com>
2022-09-12 21:39:57 +0100

    light-darkmode

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const hamburger = document.querySelector('.hamburger img');
 const close = document.querySelector('.close');
 const sidebar = document.querySelector('.sidebar');
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+const links = document.querySelectorAll('.links')
+const lightmode = document.querySelector('.light-mode');
+const darkmode = document.querySelector('.dark-mode img');
 
+
+lightmode.addEventListener('click', () => {
+
+    lightmode.style.display = 'none';
+    darkmode.style.display = 'block';
+    for (let i = 0; i < links.length; i++) {
+        links[i].classList.toggle('active');
+      }
+})
+darkmode.addEventListener('click', () => {
+    darkmode.style.display = 'none';
+   lightmode.style.display = 'block';
+   for (let i = 0; i < links.length; i++) {
+    links[i].classList.toggle('active');
+  }
+})
 
 hamburger.addEventListener('click', () => { 
    hamburger.style.display = 'none';
-            close.style.display = 'block';
-            sidebar.style.display = 'block';
+        close.style.display = 'block';
+      sidebar.style.display = 'block';
     })
 
     close.addEventListener('click', () => { 
