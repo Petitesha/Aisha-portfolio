765d09aa121cdac200d293828474118fdbc1ef1b
Bello aisha <belloaishaaderonke@gmail.com>
2022-09-14 16:23:13 +0100

    corrected-light-darkmode

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const main = document.querySelector('main');
 const links = document.querySelectorAll('.links');
 const modeIcon = document.querySelector('.modeIcon');
 const bio = document.querySelector('.hero-text');
+const messageIcon = document.querySelector('.hero-links-tag');
+const sectionTwo = document.querySelector('.section2');
+const jobs = document.querySelector('.jobs');
+const desc = document.querySelector('.desc p');
+const actions = document.querySelectorAll('.actions');
+const framework = document.querySelectorAll('.framework');
+const live = document.querySelectorAll('.live');
+const githubLogo = document.querySelectorAll('.github-button');
+const education = document.querySelector('.education');
+
 
 let mode = 'dark';
 
@@ -30,6 +40,23 @@ const changeMode = (appearance) => {
       links[i].classList.add('active');
     }
     bio.classList.add('active');
+    messageIcon.classList.add('active');
+    sectionTwo.classList.add('active');
+    jobs.classList.add('active');
+    desc.classList.add('active');
+    education.classList.add('active');
+    for (let i = 0; i < actions.length; i++) {
+      actions[i].classList.add('active');
+    }
+    for (let i = 0; i < framework.length; i++) {
+      framework[i].classList.add('active');
+    }
+    for (let i = 0; i < live.length; i++) {
+      live[i].classList.add('active');
+    }
+    for (let i = 0; i < githubLogo.length; i++) {
+      githubLogo[i].classList.add('active');
+    }
     modeIcon.src = './images/light-mode.svg';
   } else {
     main.classList.remove('active');
@@ -37,6 +64,23 @@ const changeMode = (appearance) => {
       links[i].classList.remove('active');
     }
     bio.classList.remove('active');
+    messageIcon.classList.remove('active');
+    sectionTwo.classList.remove('active');
+    jobs.classList.remove('active');
+    desc.classList.remove('active');
+    education.classList.remove('active');
+    for (let i = 0; i < actions.length; i++) {
+      actions[i].classList.remove('active');
+    }
+    for (let i = 0; i < framework.length; i++) {
+      framework[i].classList.remove('active');
+    }
+    for (let i = 0; i < live.length; i++) {
+      live[i].classList.remove('active');
+    }
+    for (let i = 0; i < githubLogo.length; i++) {
+      githubLogo[i].classList.remove('active');
+    }
     modeIcon.src = './images/dark-mode.svg';
   }
 };
